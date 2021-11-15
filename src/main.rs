bcc921ec2c6627f4d89f9a116640a047038f6ec9
Michael Delgado <mdelgado509@gmail.com>
2021-11-15 16:50:57 -0500

    Adds struct Guess and moves error handling to implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 use rand::Rng;
 use std::{cmp::Ordering, io};
 
+struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+
+    fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess a number between 1-100");
 
@@ -16,18 +34,13 @@ fn main() {
             .expect("Failed to read line");
 
         let guess: i32 = match guess.trim().parse() {
-            Ok(num) => num,
+            Ok(num) => Guess::new(num).value(),
             Err(_) => {
-                println!("Only whole numbers!");
-                continue
+                println!("Guess must be a valid integer!");
+                continue;
             }
         };
-
-        if guess < 1 || guess > 100 {
-            println!("The secret number will be between 1 and 100.");
-            continue;
-        }
-
+ 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
